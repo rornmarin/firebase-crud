626f3c541ae127032950abca5c858e7adce0dfb6
Rorn <marin.rorn@z1fsc.com>
2023-08-31 01:42:42 +0700

    commet

--- lib/components/user_card.dart
+++ lib/components/user_card.dart
@@ -1,12 +1,16 @@
-import 'package:firebase_test/add_user.dart';
 import 'package:firebase_test/edit_user.dart';
 import 'package:flutter/material.dart';
 
-class UserCard extends StatelessWidget {
+class UserCard extends StatefulWidget {
   const UserCard({
     super.key,
   });
 
+  @override
+  State<UserCard> createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
